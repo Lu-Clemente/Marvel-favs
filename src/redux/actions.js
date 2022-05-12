34eb803ca839212367edfbd068dfe07f1b391086
Luciene Clemente <luciene.clemente@unifei.edu.br>
2022-05-11 21:23:45 -0300

    Adding loading reducers

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,6 @@
 export const SET_TAB_SELECTED = "SET_TAB_SELECTED";
 export const SET_SESSION_LOGGED = "SET_SESSION_LOGGED";
+export const SET_LOADING = "SET_LOADING";
 
 export const setTabSelected = tabSelected => dispatch => {
     dispatch({
@@ -12,5 +13,12 @@ export const setSessionLogged = session_logged => dispatch => {
     dispatch({
         type: SET_SESSION_LOGGED,
         payload: session_logged
+    })
+}
+
+export const setLoading = loading => dispatch => {
+    dispatch({
+        type: SET_LOADING,
+        payload: loading
     })
 }

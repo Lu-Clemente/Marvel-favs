676888105d92b4fcdd4ff0e3a428a1244425c528
Luciene Clemente <luciene.clemente@unifei.edu.br>
2022-05-14 03:41:13 -0300

    Establishing witch data will be fetched from api

---
+++ src/services/requests/charactersIds.ts
@@ -0,0 +1,132 @@
+const results = [
+	{
+		"id": 1009165,
+		"name": "Avengers",
+	},
+	{
+		"id": 1017109,
+		"name": "Black Widow/Natasha Romanoff (MAA)",
+	},
+	{
+		"id": 1017105,
+		"name": "Captain America/Steve Rogers (MAA)",
+	},
+	{
+		"id": 1009262,
+		"name": "Daredevil",
+	},
+	{
+		"id": 1009281,
+		"name": "Doctor Doom",
+	},
+	{
+		"id": 1009276,
+		"name": "Doctor Octopus",
+	},
+	{
+		"id": 1009282,
+		"name": "Doctor Strange",
+	},
+	{
+		"id": 1009299,
+		"name": "Fantastic Four",
+	},
+	{
+		"id": 1010925,
+		"name": "Ghost Rider (Daniel Ketch)",
+	},
+	{
+		"id": 1011299,
+		"name": "Guardians of the Galaxy",
+	},
+	{
+		"id": 1017108,
+		"name": "Hawkeye/Clint Barton (MAA)",
+	},
+	{
+		"id": 1009351,
+		"name": "Hulk",
+	},
+	{
+		"id": 1017107,
+		"name": "Hulk/Bruce Banner (MAA)",
+	},
+	{
+		"id": 1009368,
+		"name": "Iron Man",
+	},
+	{
+		"id": 1017104,
+		"name": "Iron Man/Tony Stark (MAA)",
+	},
+	{
+		"id": 1009407,
+		"name": "Loki",
+	},
+	{
+		"id": 1009417,
+		"name": "Magneto",
+	},
+	{
+		"id": 1017577,
+		"name": "Ms. Marvel (Kamala Khan)",
+	},
+	{
+		"id": 1009515,
+		"name": "Punisher",
+	},
+	{
+		"id": 1009524,
+		"name": "Quicksilver",
+	},
+	{
+		"id": 1009562,
+		"name": "Scarlet Witch",
+	},
+	{
+		"id": 1009592,
+		"name": "Silver Surfer",
+	},
+	{
+		"id": 1016181,
+		"name": "Spider-Man (Miles Morales)",
+	},
+	{
+		"id": 1009610,
+		"name": "Spider-Man (Peter Parker)",
+	},
+	{
+		"id": 1010733,
+		"name": "Star-Lord (Peter Quill)",
+	},
+	{
+		"id": 1009652,
+		"name": "Thanos",
+	},
+	{
+		"id": 1009664,
+		"name": "Thor",
+	},
+	{
+		"id": 1017106,
+		"name": "Thor (MAA)",
+	},
+	{
+		"id": 1009685,
+		"name": "Ultron",
+	},
+	{
+		"id": 1009697,
+		"name": "Vision",
+	},
+	{
+		"id": 1009718,
+		"name": "Wolverine",
+	},
+	{
+		"id": 1009726,
+		"name": "X-Men",
+	}
+]
+
+export { results };
